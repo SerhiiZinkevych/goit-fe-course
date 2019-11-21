ad803c3d06a62a93f6d90661e4ac13faec09b433
Serhii Zinkevych <zinkevych@gmail.com>
2019-11-21 21:11:28 +0200

    HW_12

---
+++ JS/goit-js-hw-12/src/js/fetchCountries.js
@@ -0,0 +1,53 @@
+import listTemplate from '../templates/listTemplate.hbs';
+import singleItemTemplate from '../templates/singleItemTemplate.hbs';
+import PNotify from 'pnotify/dist/es/PNotify';
+import 'pnotify/dist/PnotifyBrightTheme.css';
+
+const debounce = require('lodash.debounce');
+const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+
+const refs = {
+  inputField: document.querySelector('#inputField'),
+  content: document.querySelector('#content'),
+};
+
+refs.inputField.addEventListener('input', debounce(getData, 500));
+
+function getData() {
+  if (!inputField.value) {
+    refs.content.innerHTML = '';
+    return;
+  }
+
+  fetch(baseUrl + inputValue)
+    .then(response => {
+      if (response.status === 404) {
+        throw 'No matches found!';
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderPage(data);
+    })
+    .catch(e => {
+      PNotify.error({
+        text: e,
+      });
+    });
+}
+
+function renderPage(data) {
+  let markup;
+  PNotify.closeAll();
+  if (data.length === 1) {
+    markup = singleItemTemplate(data[0]);
+  } else if (data.length > 10) {
+    throw 'Too many matches found. Please enter a more specific querry!';
+  } else {
+    markup = listTemplate(data);
+  }
+  refs.content.innerHTML = '';
+  if (markup) {
+    refs.content.insertAdjacentHTML('beforeend', markup);
+  }
+}
